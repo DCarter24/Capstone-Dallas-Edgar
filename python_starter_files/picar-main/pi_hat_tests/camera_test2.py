d1f3c9a06c8a84e9f1d55f3a9439ce24ff12becc
B1WAYN3 <112424397+B1WAYN3@users.noreply.github.com>
2024-09-15 16:43:42 -0500

    Update camera_test2.py

--- python_starter_files/picar-main/pi_hat_tests/camera_test2.py
+++ python_starter_files/picar-main/pi_hat_tests/camera_test2.py
@@ -8,8 +8,11 @@ cap = cv2.VideoCapture('/dev/video0', cv2.CAP_V4L)
 # cap.set(cv2.CAP_PROP_FRAME_WIDTH, 2560)
 # cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1440)
 # Changing it too 1920x1080 (Full HD)
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
+#cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+#cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
+
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
 # take frame
 ret, frame = cap.read()
