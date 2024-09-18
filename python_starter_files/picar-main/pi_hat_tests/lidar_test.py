99da03a82187ce5d990c36523060b47a8e5077d5
Dallas Carter <106203118+DCarter24@users.noreply.github.com>
2024-09-18 14:58:16 -0500

    Update lidar_test.py

--- python_starter_files/picar-main/pi_hat_tests/lidar_test.py
+++ python_starter_files/picar-main/pi_hat_tests/lidar_test.py
@@ -32,7 +32,7 @@ from adafruit_rplidar import RPLidar
 
 # Setup the RPLidar
 PORT_NAME = '/dev/ttyUSB0'
-lidar = RPLidar(None, PORT_NAME,timeout=3, baudrate=256000)
+lidar = RPLidar(None, PORT_NAME,timeout=3)
 
 # used to scale data to fit on the screen
 max_distance = 0
