14dbffb5ec87063be26f85841cc4cbae9e12f8a7
Dallas Carter <106203118+DCarter24@users.noreply.github.com>
2024-09-24 13:11:56 -0500

    Create rplidar.launch.py

---
+++ python_starter_files/picar-main/pi_hat_tests/rplidar.launch.py
@@ -0,0 +1,21 @@
+#https://github.com/joshnewans/articubot_one/blob/545acac87ae215d80ef6b28abe6097eb7281d9ff/launch/rplidar.launch.py
+import os
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+
+    return LaunchDescription([
+
+        Node(
+            package='rplidar_ros',
+            executable='rplidar_composition',
+            output='screen',
+            parameters=[{
+                'serial_port': '/dev/serial/by-path/platform-fd500000.pcie-pci-0000:01:00.0-usb-0:1.3:1.0-port0',
+                'frame_id': 'laser_frame',
+                'angle_compensate': True,
+                'scan_mode': 'Standard'
+            }]
+        )
+    ])
